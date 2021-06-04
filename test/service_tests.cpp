76113169797b213bececb8174aa38bdbb073ae34
kkulesz <kulesza.konrad@protonmail.com>
2021-06-04 19:39:18 +0200

    tidy up and add pretty ugly tests

--- test/service_tests.cpp
+++ test/service_tests.cpp
@@ -7,7 +7,7 @@
 
 BOOST_AUTO_TEST_SUITE(socket_tests)
 
-    BOOST_AUTO_TEST_CASE(case_output)
+    BOOST_AUTO_TEST_CASE(case_output_string) //10 ("aaa") tuples in server after
     {
         Tuple tuple = Tuple({"aaa"});
         auto start = std::chrono::high_resolution_clock::now();
@@ -21,49 +21,18 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
         std::cout << "Time taken by program is : " << std::fixed
              << time_taken << std::setprecision(9);
         std::cout << " sec " << std::endl;
-//        BOOST_TEST(analyzed == expected);
     }
 
-    BOOST_AUTO_TEST_CASE(case_input)
+    BOOST_AUTO_TEST_CASE(case_read_string)
     {
         TuplePattern tuplePattern = TuplePattern("string:<aab");
         auto start = std::chrono::high_resolution_clock::now();
 
         std::ios_base::sync_with_stdio(false);
         for( int i = 0; i< 10; ++i) {
-            if(std::optional<Tuple> t = linda_input(tuplePattern, 1000)){
-                std::cout<<"Input received: ";
-                t.value().print();
-                std::cout<<std::endl;
-            } else{
-                std::cout<<"Input received null: ";
-                std::cout<<std::endl;
-            }
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        double time_taken = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-        time_taken *= 1e-9;
-        std::cout << "Time taken by program is : " << std::fixed
-                  << time_taken << std::setprecision(9);
-        std::cout << " sec " << std::endl;
-//        BOOST_TEST(analyzed == expected);
-    }
-
-    BOOST_AUTO_TEST_CASE(case_read)
-    {
-        TuplePattern tuplePattern = TuplePattern("string:<aab");
-        auto start = std::chrono::high_resolution_clock::now();
-
-        std::ios_base::sync_with_stdio(false);
-        for( int i = 0; i< 10; ++i) {
-            if(std::optional<Tuple> t = linda_read(tuplePattern, 1000)){
-                std::cout<<"Read received: ";
-                t.value().print();
-                std::cout<<std::endl;
-            } else{
-                std::cout<<"Read received null: ";
-                std::cout<<std::endl;
-            }
+            std::optional<Tuple> t = linda_read(tuplePattern, 1000);
+            bool isMatch = tuplePattern.checkIfMatch(*t);
+            BOOST_TEST(isMatch);
         }
         auto end = std::chrono::high_resolution_clock::now();
         double time_taken =(double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -71,13 +40,31 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
         std::cout << "Time taken by program is : " << std::fixed
                   << time_taken << std::setprecision(9);
         std::cout << " sec " << std::endl;
-//        BOOST_TEST(analyzed == expected);
     }
 
-    BOOST_AUTO_TEST_CASE(case_output_read)
+    BOOST_AUTO_TEST_CASE(case_input_string)
     {
-        Tuple tuple = Tuple({"aaa"});
         TuplePattern tuplePattern = TuplePattern("string:<aab");
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::ios_base::sync_with_stdio(false);
+        for( int i = 0; i< 10; ++i) {
+            std::optional<Tuple> t = linda_input(tuplePattern, 1000);
+            bool isMatch = tuplePattern.checkIfMatch(*t);
+            BOOST_TEST(isMatch);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        double time_taken = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        time_taken *= 1e-9;
+        std::cout << "Time taken by program is : " << std::fixed
+                  << time_taken << std::setprecision(9);
+        std::cout << " sec " << std::endl;
+    }
+
+    BOOST_AUTO_TEST_CASE(case_output_read_integer)
+    {
+        Tuple tuple = Tuple({123});
+        TuplePattern tuplePattern = TuplePattern("integer:*");
         auto start = std::chrono::high_resolution_clock::now();
         std::ios_base::sync_with_stdio(false);
 
@@ -85,14 +72,9 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
             linda_output(tuple);
 
         for( int i = 0; i< 1; ++i) {
-            if(std::optional<Tuple> t = linda_read(tuplePattern, 0)){
-                std::cout<<"Read received: ";
-                t.value().print();
-                std::cout<<std::endl;
-            } else{
-                std::cout<<"Read received null: ";
-                std::cout<<std::endl;
-            }
+            std::optional<Tuple> t = linda_read(tuplePattern, 1000);
+            bool isMatch = tuplePattern.checkIfMatch(*t);
+            BOOST_TEST(isMatch);
         }
         auto end = std::chrono::high_resolution_clock::now();
         double time_taken =(double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -102,10 +84,10 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
         std::cout << " sec " << std::endl;
     }
 
-    BOOST_AUTO_TEST_CASE(case_output_input)
+    BOOST_AUTO_TEST_CASE(case_output_input_float)
     {
-        Tuple tuple = Tuple({"aaa"});
-        TuplePattern tuplePattern = TuplePattern("string:<aab");
+        Tuple tuple = Tuple({1.23f});
+        TuplePattern tuplePattern = TuplePattern("float:<2.0");
         auto start = std::chrono::high_resolution_clock::now();
         std::ios_base::sync_with_stdio(false);
 
@@ -113,14 +95,9 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
             linda_output(tuple);
 
         for( int i = 0; i< 1; ++i) {
-            if(std::optional<Tuple> t = linda_input(tuplePattern, 100)){
-                std::cout<<"Input received: ";
-                t.value().print();
-                std::cout<<std::endl;
-            } else{
-                std::cout<<"Input received null: ";
-                std::cout<<std::endl;
-            }
+            std::optional<Tuple> t = linda_input(tuplePattern, 1000);
+            bool isMatch = tuplePattern.checkIfMatch(*t);
+            BOOST_TEST(isMatch);
         }
         auto end = std::chrono::high_resolution_clock::now();
         double time_taken = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -128,12 +105,14 @@ BOOST_AUTO_TEST_SUITE(socket_tests)
         std::cout << "Time taken by program is : " << std::fixed
                   << time_taken << std::setprecision(9);
         std::cout << " sec " << std::endl;
-
     }
 
-    BOOST_AUTO_TEST_CASE(Error_1)
+    BOOST_AUTO_TEST_CASE(timeout)
     {
-//        BOOST_CHECK_THROW (s.check();, std::runtime_error);
+        TuplePattern tuplePattern = TuplePattern("float:<2.0; string:==lalalalalalalalaXDDDDDD; integer:<= 101010");
+        std::optional<Tuple> t = linda_input(tuplePattern, 1000);
+        bool isSuccess = t == std::nullopt;
+        BOOST_TEST(isSuccess);
     }
 
 BOOST_AUTO_TEST_SUITE_END();
